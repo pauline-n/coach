98d83f22c525f7717ca240c7b4964c55992365e1
Pauline Nalwanga <paulinon141@gmail.com>
2022-09-23 08:52:44 +0300

    first commit

---
+++ coach/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!5n4h1=d)z_0-7&*!cz^t++mg0&hqenw3&$y9$4sg1nj93or69"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ home/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, FieldRowPanel, PageChooserPanel, InlinePanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail import blocks
+from wagtail.models import Page, Orderable
+
+class HomePagePrices(Orderable):
+    page = ParentalKey('home.HomePage', on_delete=models.CASCADE, related_name='our_prices')
+    price_type = models.CharField(max_length=200)
+    price = models.CharField(max_length=200)
+    basis = models.CharField(max_length=200)
+    breakdown = RichTextField()
+    order_link = models.CharField(max_length=200)
+
+    panels = [
+        FieldPanel('price_type'),
+        FieldPanel('price'),
+        FieldPanel('basis'),
+        FieldPanel('breakdown'),
+        FieldPanel('order_link'),
+    ]
+
+class HomePage(Page):
+    b_image = models.ForeignKey('wagtailimages.Image', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
+    head_msg = models.CharField(max_length=200, default='Vincent')
+    head_body = RichTextField(default='Hello world')
+    head_btn_link = models.CharField(max_length=100, default='Free Consultation')
+    description = RichTextField(default='Hello world')
+    info = RichTextField(default='Hello world')
+    link = models.CharField(max_length=100, default='Hello world')
+    team = StreamField([
+        ('photo', ImageChooserBlock()),
+        ('name', blocks.CharBlock(form_classname="title")),
+        ('details', blocks.CharBlock(form_classname="title")),
+    ], use_json_field=True, blank=True)
+    blog_feature = models.ForeignKey(Page,  null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',)
+    contact_phone = models.CharField(max_length=200, default ='256774559687')
+    contact_email = models.EmailField(default='ojaravincent5@gmail.com')
+    contact_location = models.CharField(max_length=255, default='Kampala, Uganda')
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('b_image'),
+            FieldPanel('head_msg'),
+            FieldPanel('head_body'),
+            FieldPanel('head_btn_link'),
+        ], heading='Home header section' ),
+        FieldPanel('description'),
+        MultiFieldPanel([
+            FieldPanel('info'),
+            FieldPanel('link')
+        ], heading='Brief information about us'),
+        InlinePanel('our_prices', label='Pricing', max_num=3, min_num=1),
+        FieldPanel('team'),
+        PageChooserPanel('blog_feature'),
+        MultiFieldPanel([
+            FieldPanel('contact_phone'),
+            FieldPanel('contact_email'),
+            FieldPanel('contact_location'),
+        ])
+    ]
+
+
